ec3dd6c89c48a0da6adc180ba0eb4624b84e3e2d
carol <carol@10.0.0.241>
2025-01-29 20:58:35 -0500

    implemented add()

--- src/demo1/DemoTest5.java
+++ src/demo1/DemoTest5.java
@@ -3,10 +3,20 @@ package demo1;
 public class DemoTest5 {
 	
 	public static void main (String args[]) {
-		System.out.println("Java");
+		System.out.println("Batch13:");
 		
+		
+		int a = 10;
+		int b = 20;
+		
+		int result = a + b;
+		
+		System.out.println(result);
 		
 	}
 	
+	public void add(int a, int b) {
+		System.out.println(a+b);
+	}
 
 }
